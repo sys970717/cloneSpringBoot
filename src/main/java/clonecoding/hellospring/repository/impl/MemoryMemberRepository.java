f6e8f9b96802d613b2ec6038336d0475d40a3c47
sys970717 <sys970717@naver.com>
2021-07-28 13:08:42 +0900

    12강 강의 내용 따라 코딩하기. 테스트 케이스 작성법.

--- src/main/java/clonecoding/hellospring/repository/impl/MemoryMemberRepository.java
+++ src/main/java/clonecoding/hellospring/repository/impl/MemoryMemberRepository.java
@@ -13,7 +13,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Member save(Member member) {
-        member.setId(sequence);
+        member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
     }
@@ -34,4 +34,7 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() { store.clear(); }
+
 }
