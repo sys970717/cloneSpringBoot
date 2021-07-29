a30e41c1b1360ca1928beeaffa3895227ce538b0
sys970717 <sys970717@naver.com>
2021-07-30 00:24:42 +0900

    스프링 컨테이너에 객체 생성하기. 컴포넌트 스캔을 통해 객체를 생성하고 의존성을 주입함. 스프링 컨테이너의 객체는 싱글톤이다.

--- src/main/java/clonecoding/hellospring/service/MemberService.java
+++ src/main/java/clonecoding/hellospring/service/MemberService.java
@@ -3,15 +3,33 @@ package clonecoding.hellospring.service;
 import clonecoding.hellospring.domain.Member;
 import clonecoding.hellospring.repository.MemberRepository;
 import clonecoding.hellospring.repository.impl.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * 현재는 순수 자바코드이다.
+ * 스프링 컨테이너에 등록이 되지않았음.
+ * public class MemberService {
+ */
+
+// 스프링 컨테이너에 등록한다.
+@Service
+/**
+ * @Service
+ * 컴포넌트 스캔
+ * @Component
+ * 컴포넌트 어논테이션은 service 내에 있어, 컴포넌트 스캔때문에 스프링 컨테이너에 등록함.
+ */
 public class MemberService {
     private final MemberRepository memberRepository;
 
     // 외부에서의 주입 ( Dependency Injection )
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
 
