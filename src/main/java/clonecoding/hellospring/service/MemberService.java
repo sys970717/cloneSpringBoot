6c5bf016aa1e2520efd737c5af8698be41ce3510
sys970717 <sys970717@naver.com>
2021-07-29 23:30:27 +0900

    회원 서비스로직 만들기.

---
+++ src/main/java/clonecoding/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package clonecoding.hellospring.service;
+
+import clonecoding.hellospring.domain.Member;
+import clonecoding.hellospring.repository.MemberRepository;
+import clonecoding.hellospring.repository.impl.MemoryMemberRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입.
+     * 
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        // TODO 같은 이름이 있는 중복 회원 x
+        validateDuplicate(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicate(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id) {
+        return memberRepository.findById(id);
+    }
+
+}
